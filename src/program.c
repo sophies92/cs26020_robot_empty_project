a3d9c907d6f5bda653b35fd3c7c3726938e585ea
sophie short <sos66@aber.ac.uk>
2025-02-09 18:29:39 +0000

    init

---
+++ src/program.c
@@ -0,0 +1,20 @@
+/** 
+ * @file program.c
+ * @author
+ * @created
+ * @edited
+*/
+#include "../lib/allcode_api.h"
+
+/**
+ * The entry point for the program
+ */
+int main()
+{
+    FA_RobotInit();
+
+    while(true)
+    {
+
+    }
+}
